0c5751e87f78c3b51e396456ce3b7c39e7a9eba2
Jimmy Blundell <jblundell123@gmail.com>
2020-10-27 13:21:34 -0500

    test commit

--- HelloWorldApp/app/src/main/java/com/csee5590/helloworldapp/MainActivity.java
+++ HelloWorldApp/app/src/main/java/com/csee5590/helloworldapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.csee5590.helloworldapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+// test comment
 public class MainActivity extends AppCompatActivity {
 
     @Override
